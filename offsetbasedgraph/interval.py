48f422ec0231048015aeebb69a9dd0965fd9ec31
Ivar Grytten <ivar@1337.no>
2016-12-02 12:14:50 +0100

    testInterval

--- offsetbasedgraph/interval.py
+++ offsetbasedgraph/interval.py
@@ -1,4 +1,13 @@
 
+class Position(object):
+    """
+    Represents a position  in the graph
+    """
+    def __init__(self, region_path_id, offset):
+        self.region_path_id = region_path_id
+        self.offset = offset
+
+
 class Interval(object):
     start_position = None
     end_position = None
@@ -6,3 +15,8 @@ class Interval(object):
 
     def length(self):
         pass
+
+    def __init__(self, start_position, end_position, region_paths):
+        self.start_position = start_position
+        self.end_position = end_position
+        self.region_paths = region_paths
