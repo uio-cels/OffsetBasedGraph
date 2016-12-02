dcf940f0ea38b58846165b3f1e45ee914941a374
Ivar Grytten <ivar@1337.no>
2016-12-02 11:44:38 +0100

    Prototyping

--- offsetbasedgraph/__init__.py
+++ offsetbasedgraph/__init__.py
@@ -3,3 +3,6 @@ from .regionpath import RegionPath
 from .linearinterval import LinearInterval
 from .DbWrapper import DbWrapper
 from .graphinterval import GraphInterval
+from .graph import Graph
+from .interval import Interval
+from .graph import Block
