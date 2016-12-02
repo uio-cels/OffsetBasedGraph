78f1d4693b49b93ed81e2ee1fe831709d04da547
Knut Rand <knutdrand@gmail.com>
2016-12-02 16:28:09 +0100

    forced

--- tests/testInterval.py
+++ tests/testInterval.py
@@ -6,18 +6,20 @@ from offsetbasedgraph import Interval, Position
 class TestInterval(unittest.TestCase):
 
     def testSimpelInterval(self):
-        graph = dummygraph.get_simple_graph()
         region_paths = [1, 3, 4]
         interval = Interval(Position(1, 10), Position(4, 10), region_paths)
 
         for r in region_paths:
-            self.assertTrue(r in interval.region_paths,
-                    "The region path %d is not in interval's region paths")
+            self.assertTrue(
+                r in interval.region_paths,
+                "The region path %d is not in interval's region paths")
 
         self.assertEqual(len(interval.region_paths), 3,
-                        "Interval should have 3 region paths")
+                         "Interval should have 3 region paths")
 
+
+    def test_interval_length(self):
+        
 
 if __name__ == "__main__":
     unittest.main()
-
