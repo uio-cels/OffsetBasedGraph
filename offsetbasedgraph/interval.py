39c2c4b07798976545e1b68e3df366b6ba39d3a1
Knut Rand <knutdrand@gmail.com>
2016-12-07 11:23:05 +0100

    cleanup

--- offsetbasedgraph/interval.py
+++ offsetbasedgraph/interval.py
@@ -101,7 +101,6 @@ class Interval(object):
         self.region_paths = region_paths
         self.graph = graph
 
-
         # Sanity check interval
         assert self.start_position.region_path_id in self.region_paths
         assert self.end_position.region_path_id in self.region_paths
@@ -112,10 +111,13 @@ class Interval(object):
             assert rp in graph.blocks, "Region path %s not in graph" % rp
 
         # Check offsets
-        assert self.end_position.offset <= graph.blocks[self.region_paths[-1]].length(), \
-            "Offset %d in block %d with size %d" % (self.end_position.offset,
-                                                    self.region_paths[-1],
-                                                    graph.blocks[self.region_paths[-1]].length())
+        max_offset = graph.blocks[self.region_paths[-1]].length()
+        msg = "Offset %d in block %d with size %d" % (
+            self.end_position.offset,
+            self.region_paths[-1],
+            graph.blocks[self.region_paths[-1]].length())
+
+        assert self.end_position.offset <= max_offset, msg
 
     def __eq__(self, other):
         eq = self.start_position == other.start_position
