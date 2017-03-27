bc7fba24dd79fdc1f442c6d1fe9e36b45d5c7ee1
ivar <ivar@ivar>
2017-03-27 13:33:38 +0200

    version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='offsetbasedgraph',
-      version='1.0.3',
+      version='1.0.4',
       description='Offset based graph',
       url='http://github.com/uio-cels/offsetbasedgraph',
       author='Ivar Grytten and Knut Rand',
@@ -19,9 +19,9 @@ setup(name='offsetbasedgraph',
 
 """
 To update package:
-#Update version
+#Update version number manually in this file
 
 sudo python3 setup.py sdist
 sudo python3 setup.py bdist_wheel
-twine upload dist/*
+twine upload dist/offsetbasedgraph-VERSION.tar.gz
 """
