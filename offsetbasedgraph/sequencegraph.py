6ebe8e26ed296bf1986e790b27046461c81b9307
Ivar Grytten <ivar.grytten@gmail.com>
2018-03-11 21:16:57 +0100

    New Sequencegraph for storing sequence of graph

---
+++ offsetbasedgraph/sequencegraph.py
@@ -0,0 +1,138 @@
+from .graph import BlockArray
+import numpy as np
+import json
+import logging
+
+class SequenceGraph():
+    _compliments = {"A": "T",
+                    "a": "t",
+                    "C": "G",
+                    "c": "g",
+                    "T": "A",
+                    "t": "a",
+                    "G": "C",
+                    "g": "c"}
+
+    _letters = np.array(["n", "a", "c", "t", "g"])
+
+    def __init__(self, node_id_offset, indices, node_sizes, sequence_array):
+        self._indices = indices
+        self._node_id_offset = node_id_offset
+        self._node_sizes = node_sizes
+        self._sequence_array = sequence_array
+
+    def to_file(self, file_name):
+        file = open(file_name, "wb")
+        np.savez_compressed(file,
+                            node_id_offset=self._node_id_offset,
+                            indices=self._indices,
+                            node_sizes=self._node_sizes,
+                            sequence_array=self._sequence_array)
+        file.close()
+
+    @classmethod
+    def from_file(cls, file_name):
+        file = open(file_name, "rb")
+        data = np.load(file)
+        seqgraph = cls(data["node_id_offset"],
+                       data["indices"],
+                       data["node_sizes"],
+                       data["sequence_array"])
+        file.close()
+        return seqgraph
+
+    @classmethod
+    def create_empty_from_ob_graph(cls, ob_graph):
+        assert isinstance(ob_graph.blocks, BlockArray), "Block backend must be BlockArray"
+        blocks = ob_graph.blocks
+        node_sizes = ob_graph.blocks._array
+        indices = np.cumsum(node_sizes)
+        indices = np.insert(indices, 0, 0)
+        sequence_array = np.zeros(np.sum(node_sizes), dtype=np.uint8)
+        return cls(ob_graph.blocks.node_id_offset, indices, node_sizes, sequence_array)
+
+    def set_sequences_using_vg_json_graph(self, file_name):
+        f = open(file_name)
+        i = 1
+        for line in f.readlines():
+            line = json.loads(line)
+            if "node" in line:
+                node_objects = line["node"]
+                for node_object in node_objects:
+                    i += 1
+                    if i % 1000000 == 0:
+                        logging.info("%d nodes processed" % i)
+                    self.set_sequence(int(node_object["id"]), node_object["sequence"])
+
+    def _letter_sequence_to_numeric(self, sequence):
+        sequence[np.where(sequence == "n")[0]] = 0
+        sequence[np.where(sequence == "a")[0]] = 1
+        sequence[np.where(sequence == "c")[0]] = 2
+        sequence[np.where(sequence == "t")[0]] = 3
+        sequence[np.where(sequence == "g")[0]] = 4
+        return sequence
+
+    def set_sequence(self, node_id, sequence):
+        sequence = sequence.lower()
+        index = node_id - self._node_id_offset
+        node_size = self._node_sizes[index]
+        assert node_size == len(sequence), "Invalid sequence. Does not cover whole node"
+        sequence = np.array(list(sequence))
+        sequence = self._letter_sequence_to_numeric(sequence)
+        pos = self._indices[index]
+        self._sequence_array[pos:pos+node_size] = sequence
+
+    def get_sequence_on_directed_node(self, node_id, start=0, end=False):
+        """Handles directed nodes"""
+        if node_id > 0:
+            return self.get_sequence(node_id, start, end)
+        else:
+            node_size = len(self.get_sequence(-node_id))
+
+            if not end:
+                end = node_size
+
+            new_start = node_size - end
+            new_end = node_size - start
+            reverse_sequence = self.get_sequence(-node_id, new_start, new_end)
+            return self._reverse_compliment(reverse_sequence)
+
+    def get_sequence(self, node_id, start=0, end=False):
+        index = node_id - self._node_id_offset
+        pos = self._indices[index]
+        node_size = self._node_sizes[index]
+        sequence = self._sequence_array[pos:pos+node_size]
+
+        if not end:
+            end = node_size
+
+        return ''.join(self._letters[sequence[start:end]])
+
+    def _reverse_compliment(self, sequenence):
+        return "".join(self._compliments[c] for c in sequenence[::-1])
+
+    def get_interval_sequence(self, interval):
+
+        rps = interval.region_paths
+        start_node = rps[0]
+        end_node = rps[-1]
+
+        if start_node == end_node and len(rps) == 1:
+            return self.get_sequence_on_directed_node(
+                start_node,
+                interval.start_position.offset,
+                interval.end_position.offset)
+        else:
+
+            start_sequence = self.get_sequence_on_directed_node(
+                start_node,
+                interval.start_position.offset)
+            end_sequence = self.get_sequence_on_directed_node(
+                end_node,
+                0,
+                interval.end_position.offset)
+            middle_sequence = ""
+            for rp in rps[1:-1]:
+                middle_sequence += self.get_sequence_on_directed_node(rp)
+
+            return "%s%s%s" % (start_sequence, middle_sequence, end_sequence)
---
+++ tests/test_sequencegraph.py
@@ -0,0 +1,105 @@
+import unittest
+from offsetbasedgraph import Graph, Block, Interval
+from offsetbasedgraph.sequencegraph import SequenceGraph
+
+class TestSequenceGraph(unittest.TestCase):
+
+    def setUp(self):
+        self.obgraph = Graph(
+            {3: Block(4),
+             4: Block(2),
+             5: Block(5)}
+            ,
+            {
+                3: [4],
+                4: [5]
+            }
+        )
+        self.obgraph.convert_to_numpy_backend()
+
+        self.sequencegraph = SequenceGraph.create_empty_from_ob_graph(self.obgraph)
+
+        self.obgraph2 = Graph(
+            {1: Block(3),
+             2: Block(3),
+             3: Block(3)}
+            ,
+            {
+                1: [2],
+                2: [3]
+            }
+        )
+        self.obgraph2.convert_to_numpy_backend()
+        self.sequencegraph2 = SequenceGraph.create_empty_from_ob_graph(self.obgraph2)
+        self.sequencegraph2.set_sequence(1, "aag")
+        self.sequencegraph2.set_sequence(2, "gaa")
+        self.sequencegraph2.set_sequence(3, "aga")
+        self.retriever = self.sequencegraph2
+
+    def test_set_get_sequence(self):
+        self.sequencegraph.set_sequence(3, "aacc")
+        seq = self.sequencegraph.get_sequence(3)
+        self.assertEqual(seq, "aacc")
+
+    def test_set_get_subsequence(self):
+        self.sequencegraph.set_sequence(3, "aacc")
+        seq = self.sequencegraph.get_sequence(3, 1)
+        self.assertEqual(seq, "acc")
+
+        seq = self.sequencegraph.get_sequence(3, 1, 2)
+        self.assertEqual(seq, "a")
+
+        seq = self.sequencegraph.get_sequence(3, 2, 3)
+        self.assertEqual(seq, "c")
+
+    def test_single_node_interval(self):
+        interval = Interval(0, 3, [1])
+        self.assertEqual(self.retriever.get_interval_sequence(interval),
+                         "aag")
+
+    def test_single_node_interval_with_offset(self):
+        interval = Interval(1, 3, [1])
+        self.assertEqual(self.retriever.get_interval_sequence(interval),
+                         "ag")
+
+    def test_single_node_interval_with_dual_offset(self):
+        interval = Interval(1, 2, [1])
+        self.assertEqual(self.retriever.get_interval_sequence(interval),
+                         "a")
+
+    def test_reversed_single_node_interval(self):
+        interval = Interval(0, 3, [-1])
+        self.assertEqual(self.retriever.get_interval_sequence(interval),
+                         "ctt")
+
+    def test_reversed_single_node_interval_with_dual_offsetl(self):
+        interval = Interval(1, 2, [-3])
+        self.assertEqual(self.retriever.get_interval_sequence(interval),
+                         "c")
+
+    def test_multiple_nodes_interval(self):
+        interval = Interval(0, 3, [1, 2])
+        self.assertEqual(self.retriever.get_interval_sequence(interval),
+                         "aaggaa")
+
+    def test_multiple_nodes_interval_second_rp_reversed(self):
+        interval = Interval(0, 3, [1, -2])
+        self.assertEqual(self.retriever.get_interval_sequence(interval),
+                         "aagttc")
+
+    def test_long_interval(self):
+        interval = Interval(1, 1, [1, 2, 3, -3, -2, -1])
+        self.assertEqual(self.retriever.get_interval_sequence(interval),
+                         "aggaaagatctttcc")
+
+    def test_to_from_file(self):
+        self.sequencegraph.to_file("test.sequencegraph")
+        other = SequenceGraph.from_file("test.sequencegraph")
+
+        self.assertEqual(self.sequencegraph.get_sequence(3), other.get_sequence(3))
+        self.assertEqual(self.sequencegraph.get_sequence(4), other.get_sequence(4))
+        self.assertEqual(self.sequencegraph.get_sequence(5), other.get_sequence(5))
+
+
+if __name__ == "__main__":
+    unittest.main()
