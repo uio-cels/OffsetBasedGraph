abcffeac81bd7998a14ecce967e98e7aa749b28c
Knut Rand <knutdrand@gmail.com>
2019-02-06 14:06:31 +0100

    passing insertion test

--- tests/test_vcfgraph.py
+++ tests/test_vcfgraph.py
@@ -14,3 +14,10 @@ def test_simple_insertion():
     true_graph = VCFGraph([3, 3, 7], AdjList([0, 2, 3, 3], [1, 2, 2]), SNPs())
     assert graph == true_graph
 
+
+def test_two_insertion():
+    graph = graph_from_indels(np.array([[], []]), np.array([[2, 4], [3, 2]]), 10)
+    adj_list = AdjList.from_dict({0: [1, 2], 1: [2], 2: [3, 4], 3: [4]}, 5)
+    true_graph = VCFGraph([3, 3, 2, 2, 5], adj_list, SNPs())
+    assert graph == true_graph
+
